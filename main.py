f179f52f8ca1c2aba064fb926970caa3a1fb6658
rirezaulislam2001-bit <rirezaulislam2001@gmail.com>
2026-07-26 21:14:30 +0600

    Correct aiogram import syntax in main.py
    Fix import statement for aiogram in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+a aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 import asyncio
