d622ee06fc0244f5bf01e0daff311aeb0f8c3f8e
rirezaulislam2001-bit <rirezaulislam2001@gmail.com>
2026-07-26 21:10:26 +0600

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(Command("start"))
+async def start(message: Message):
+    await message.answer("🎉 Welcome to REFERXTASKMASTER_BOT")
