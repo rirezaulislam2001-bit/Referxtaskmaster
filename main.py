0deeb53a547a02fec8458def6eaf5287c8acc0b2
rirezaulislam2001-bit <rirezaulislam2001@gmail.com>
2026-07-27 21:51:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from handlers.deposit import router as deposit_router
 from handlers.admin import router as admin_router
 from handlers.profile import router as profile_router
 
-TOKEN = os.getenv 8941867493:AAHjtsFqqufIl3XxBbHel0VrdUhtA0qPDXs
+TOKEN = 8941867493:AAHjtsFqqufIl3XxBbHel0VrdUhtA0qPDXs
 bot = Bot 8941867493:AAHjtsFqqufIl3XxBbHel0VrdUhtA0qPDXs
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
