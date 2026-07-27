abeea7cf1b811393964b72fb78ddf764c5904306
rirezaulislam2001-bit <rirezaulislam2001@gmail.com>
2026-07-27 21:36:01 +0600

    Change bot token in main.py
    Updated the bot token for security reasons.

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from handlers.deposit import router as deposit_router
 from handlers.admin import router as admin_router
 from handlers.profile import router as profile_router
 
-TOKEN = os.getenv 8941867493:AAEhZBRYoQ5XkKTbcroFtGZEMApBRgXo9Wg
+TOKEN = os.getenv 8941867493:AAFWqKa0ZnBZtCcaETjBKlZPjOU7IZG0fSs
 
-bot = Bot 8941867493:AAEhZBRYoQ5XkKTbcroFtGZEMApBRgXo9Wg
+bot = Bot 8941867493:AAFWqKa0ZnBZtCcaETjBKlZPjOU7IZG0fSs
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
