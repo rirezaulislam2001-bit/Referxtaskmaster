de2321259eb743514f239c1c7d4a8a80f197d364
rirezaulislam2001-bit <rirezaulislam2001@gmail.com>
2026-07-27 21:18:46 +0600

    Update states.py

--- states.py
+++ states.py
@@ -1,6 +1,13 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class DepositState(StatesGroup):
     amount = State()
     trxid = State()
     screenshot = State()
+
+
+class WithdrawState(StatesGroup):
+    amount = State()
+    method = State()
+    number = State()
